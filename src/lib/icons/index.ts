5eb96ba12e50140f40b9bbbe11546f0bed3fdcff
Rachel Hall <rachelhall@Rachels-MBP.attlocal.net>
2023-09-14 01:09:06 -0700

    starting to look good

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,11 @@
+import facebook from './facebook.svg'
+import instagram from './instagram.svg'
+import spotify from './spotify.svg'
+import raspberry from './raspberry_new_color.svg'
+
+export {
+    facebook,
+    instagram,
+    spotify,
+    raspberry
+}
