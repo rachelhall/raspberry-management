ddd4c8a72e4c0e88c17437e0838b1b3ccd8163b8
Rachel Hall <rachelhall@Rachels-MBP.attlocal.net>
2023-09-13 19:43:11 -0700

    cloudinary storage

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -4,6 +4,6 @@ export const artists = {
         bio: ` Mariela's indie rock sensibilities have been compared to Jenny Lewis, The Aces and
                 Soccer Mommy. They've played all across the southeast while they were based in Nashville before moving out to San Diego in January 2022.
                 Since then, the band has been picking up steam in southern California, playing San Diego Pride Festival, SheFest and most rececntly the Idyllwild Flim Flam Festival.`,
-        src: "public/mariela.jpg"
+        src: "https://res.cloudinary.com/dyspjkmgs/image/upload/v1694659258/mariela_hb0vdq.jpg"
     }
 }
