2ec657fffdc07c1fc0cba5035a94eff241bdf1dd
Rachel Hall <rachelhall@Rachels-MBP.attlocal.net>
2023-09-13 17:54:40 -0700

    quick fixes

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -1,11 +1,9 @@
-import { type Artist } from "../lib/types/artist"
-
-export const artists: Artist[] = [
-    {
+export const artists = {
+    mariela: {
         name: "Mariela",
         bio: ` Mariela's indie rock sensibilities have been compared to Jenny Lewis, The Aces and
-Soccer Mommy. They've played all across the southeast while they were based in Nashville before moving out to San Diego in January 2022.
-Since then, the band has been picking up steam in southern California, playing San Diego Pride Festival, SheFest and most rececntly the Flim Flam Festival in Idyllwild.`,
+                Soccer Mommy. They've played all across the southeast while they were based in Nashville before moving out to San Diego in January 2022.
+                Since then, the band has been picking up steam in southern California, playing San Diego Pride Festival, SheFest and most rececntly the Idyllwild Flim Flam Festival.`,
         src: "public/mariela.jpg"
     }
-]
+}
